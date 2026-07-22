4f276b037d5c7a02bf0b6d192e470cea6434be12
krish kumar <k.singh.codebase@gmail.com>
2026-07-23 00:18:51 +0530

    jwt updation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -116,9 +116,9 @@ const loginUser = async (req, res) => {
 
         res.cookie("access_token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
         user.lastLogin = new Date();
